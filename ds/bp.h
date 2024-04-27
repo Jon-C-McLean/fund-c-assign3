b803cab700035620e96b2909973aa0aa4ebe48f4
Jon McLean <jon@mclean.one>
2024-04-27 22:27:24 +1000

    Added documentation in DB and BP header files
    DB = Database, BP = B+ Tree

--- ds/bp.h
+++ ds/bp.h
@@ -29,9 +29,43 @@ typedef struct {
     bp_node_t *root;
 } bp_tree_t;
 
+/**
+ * @brief Create a new B+ Tree
+ * 
+ * @return bp_tree_t* The resulting B+ Tree
+ */
 bp_tree_t* BPT_CreateTree(void);
+
+/**
+ * @brief Free a B+ Tree and all associated memory
+ * 
+ * @param[in] tree The tree to free
+ * @return int Status of the operation
+ */
 int BPT_FreeTree(bp_tree_t *tree);
+
+/**
+ * @brief Search for a key in the B+ Tree
+ * 
+ * @param[in] tree The tree to search
+ * @param[in] key The key to search for
+ * @return bp_entry_t* The entry found, or NULL if not found
+ */
 bp_entry_t* BPT_SearchKey(bp_tree_t *tree, int key);
+
+/**
+ * @brief Insert a new key into the B+ Tree
+ * 
+ * @param[in] tree The tree to insert into
+ * @param[in] key The key to insert
+ * @param[in] value The value to insert
+ * @param[in] length The length of the value
+ */
 void BPT_Insert(bp_tree_t *tree, int key, char* value, uint64_t length);
 
+/**
+ * @brief Display the B+ Tree
+ * @note This is for debugging purposes only
+ * @param[in] tree The tree to display
+ */
 void BPT_DisplayTree(bp_tree_t *tree);
--- ds/db.h
+++ ds/db.h
@@ -20,18 +20,115 @@ typedef struct {
     table_t *tables;
 } database_t;
 
+/**
+ * @brief Creates a new database with the default schema and table.
+ * 
+ * @param[inout] db Resulting database
+ * @return status_t Status of the operation
+ */
 status_t DB_CreateDefaultDatabase(database_t **db);
+
+/**
+ * @brief Destroys a database and frees all memory associated with it.
+ * 
+ * @warning This operation will free all memory associated with the database
+ * and the associated schema. This operation will not save the database to disk.
+ * 
+ * @param[inout] db The database to destroy.
+ * @return status_t Status of the operation
+ */
 status_t DB_DestroyDatabase(database_t *db);
 
+/**
+ * @brief Creates a new table in the database with the specified columns.
+ * 
+ * @note This function will handle schema creation and updating.
+ * 
+ * @param[inout] db The database to create the table in.
+ * @param[in] name The name of the table to create.
+ * @param[in] columns The columns to create in the table.
+ * @param[in] numColumns The number of columns in the table.
+ * @return status_t Status of the operation
+ */
 status_t DB_CreateTable(database_t *db, char *name, table_col_def_t *columns, int numColumns);
+
+/**
+ * @brief Drops a table from the database.
+ * 
+ * @note This function will free all memory associated with the table and remove
+ * it from the schema.
+ * 
+ * @param[inout] db The database to drop the table from.
+ * @param[in] name The name of the table to drop.
+ * @return status_t Status of the operation
+ */
 status_t DB_DropTable(database_t *db, char *name);
 
+/**
+ * @brief Inserts a row into the specified table.
+ * 
+ * @param[inout] db The database to insert the row into.
+ * @param[in] tableName The name of the table to insert the row into.
+ * @param[in] values The values to insert into the row.
+ * @return status_t Status of the operation
+ */
 status_t DB_InsertRow(database_t *db, char *tableName, void **values);
+
+/**
+ * @brief Deletes a row from the specified table.
+ * 
+ * @param[inout] db The database to delete the row from.
+ * @param[in] tableName The name of the table to delete the row from.
+ * @param[in] rowId The ID of the row to delete.
+ * @return status_t Status of the operation
+ */
 status_t DB_DeleteRow(database_t *db, char *tableName, int rowId);
 
+/**
+ * @brief Updates a row in the specified table.
+ * 
+ * @param[inout] db The database to update the row in.
+ * @param[in] tableName The name of the table to update the row in.
+ * @param[in] rowId The ID of the row to update.
+ * @param[in] values The new values for the row.
+ * @return status_t Status of the operation
+ */
 status_t DB_UpdateRow(database_t *db, char *tableName, int rowId, void **values);
 
+/**
+ * @brief Selects a row from the specified table.
+ * 
+ * @param[in] db The database to select the row from.
+ * @param[in] tableName The name of the table to select the row from.
+ * @param[in] rowId The ID of the row to select.
+ * @param[out] values The values of the row.
+ * @return status_t Status of the operation
+ */
+status_t DB_SelectRow(database_t *db, char *tableName, int rowId, void **values);
+
+/* TODO: Search/select functions */
+
+/**
+ * @brief Saves the database to disk.
+ * 
+ * @param[in] db The database to save.
+ * @param[in] filename The name of the file to save the database to.
+ * @param[in] compress Whether or not to compress the database.
+ * @param[in] key The key to encrypt the database with.
+ * @param[in] keySize The size of the key (only supports 128 bit).
+ * @return status_t Status of the operation
+ */
 status_t DB_SaveDatabase(database_t *db, char *filename, int compress, char *key, int keySize);
+
+/**
+ * @brief Loads a database from disk.
+ * 
+ * @param[inout] db The database to load the data into.
+ * @param[in] filename The name of the file to load the database from.
+ * @param[in] key The key to decrypt the database with.
+ * @param[in] keySize The size of the key (only supports 128 bit).
+ * @return status_t Status of the operation
+ */
 status_t DB_LoadFromDisk(database_t *db, char *filename, char *key, int keySize);
 
 #endif
